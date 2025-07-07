aefa3fbe22e25c350c3fd3b46e6f17992af287d8
Satish Reddy <yettapusatishreddy@email.com>
2025-07-07 10:39:50 +0530

    Alert Messages Developed Successfully

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import "./App.css";
 import ClassComponent from "./ClassComponent";
 import MyComponent from "./MyComponent";
 import Counter from "./components/Counter/Counter";
+import Alert from "./components/Alerts/Alert";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Counter />
+      <Alert />
     </>
   );
 }
