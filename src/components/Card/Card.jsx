bb34b698705cd8a3d747accf592ad62b9f1670ba
Satish Reddy <yettapusatishreddy@email.com>
2025-07-15 12:12:26 +0530

    Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,14 +3,16 @@ import Trainer from "../../assets/trainer.jpg";
 import "./card.css";
 const card = () => {
   return (
-    <div className="h-[350px] w-[25%] bg-red-300 p-[1%] box-border">
+    <div className="h-[auto] w-[25%] p-[1%] box-border shadow flex flex-col justify-between gap-2">
       <img src={Trainer} alt="" className="h-[200px] w-[100%]" />
       <h3 className="text-xl font-semibold">Singe</h3>
       <p className="text-[16px] font-extralight">MA Trampsd</p>
-      <p className="text-[16px] from-neutral-400">
+      <p className="text-[14px] font-light text-gray-600">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </p>
-      <a href="">LinkedIn Profile</a>
+      <a href="" className="text-[16px] font-light text-blue-950">
+        LinkedIn Profile
+      </a>
     </div>
   );
 };
